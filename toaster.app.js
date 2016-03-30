c18b51c78470e43f71b3408a6974a6ae0d043387
verebelyicsaba <csaba.verebelyi@gmail.com>
2016-03-30 22:44:38 +0200

    Initial commit

---
+++ toaster.app.js
@@ -0,0 +1,8 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('wrms-toaster', [
+            'ngMaterial'
+        ]);
+})();
---
+++ toaster.js
@@ -0,0 +1,42 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('wrms-toaster')
+        .factory('toasterService', toasterService);
+
+    toasterService.$inject = ['$mdToast'];
+
+    /* @ngInject */
+    function toasterService($mdToast) {
+        var delay = 2000;
+        var position = 'bottom right';
+        var service = {
+            simple: simple,
+            action: action
+        };
+
+        return service;
+
+        function simple(content) {
+            return show(build(content));
+        }
+
+        function action(content, action, callback) {
+            return show(
+                build(content).action(action)
+            ).then(callback);
+        }
+
+        function show(options) {
+            return $mdToast.show(options);
+        }
+
+        function build(content) {
+            return $mdToast.simple()
+                .textContent(content)
+                .position(position)
+                .hideDelay(delay);
+        }
+    }
+})();
